bb7a08afff4c24ff2ec9755e3d3ad0376a6ec874
Mericimuu <mericozkan95@gmail.com>
2021-04-08 11:54:38 +0300

    inheritance and interfaces
    inharitance implementations

---
+++ Interfaces/Interfaces/ICustomerDal.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Interfaces
+{
+    //data access layer
+    interface ICustomerDal
+    {
+        void Add();
+        void Update();
+        void Delete();
+    }
+
+    class SqlServerCustomerDal : ICustomerDal
+    {
+        public void Add()
+        {
+            Console.WriteLine("Sql Added!");
+        }
+
+        public void Delete()
+        {
+            Console.WriteLine("Sql Deleted!");
+        }
+
+        public void Update()
+        {
+            Console.WriteLine("Sql Updated!");
+        }
+    }
+    class OracleCustomerDal : ICustomerDal
+    {
+        public void Add()
+        {
+            Console.WriteLine("Oracle Added!");
+        }
+
+        public void Delete()
+        {
+            Console.WriteLine("Oracle Deleted!");
+        }
+
+        public void Update()
+        {
+            Console.WriteLine("Oracle Updated!");
+        }
+    }
+
+    class CustomerManager
+    {
+        /*burayı oracle ya da sql'e göre verirsek, günün birinde proje ilerlediği zaman bağımlı hale gelmiş oluruz.
+        Proje sadece sql ile ya da oracle ile çalışıyor dememek için interface tanımına ihtiyaç duyarız. */
+
+        public void Add(ICustomerDal customerDal)
+        {
+            customerDal.Add();
+        }
+    }
+
+
+}
---
+++ Interfaces/Interfaces/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+
+namespace Interfaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //  InterfacesIntro();
+            //Demo();
+
+            ICustomerDal[] customerDals = new ICustomerDal[2]
+            {
+                new SqlServerCustomerDal(),
+                new OracleCustomerDal()
+            };
+
+            foreach(var customerDal in customerDals)
+            {
+                customerDal.Add();
+            }
+            Console.ReadLine();
+        }
+        private static void Demo()
+        {
+            //interface'in tek başına instance'ı oluşturulamaz.
+            // IPerson person = new IPerson(); hatalı. new IPerson yerine, customer, student vs olur.
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add(new SqlServerCustomerDal());
+        }
+
+        private static void InterfacesIntro()
+        {
+            PersonManager manager = new PersonManager();
+            //uzun yol aşağıda belirtilmiştir, kısaca manager.add diyerek de içeriğini verbiliriz.
+            //Customer customer = new Customer
+            //{
+            //    Id = 1,
+            //    FirstName = "Meric",
+            //    LastName = "Ozkan",
+            //    Address = "Istanbul"
+            //};
+            // manager.Add(customer);
+            manager.Add(new Customer { Id = 1, FirstName = "Meric", LastName = "Ozkan", Address = "Istanbul" });
+
+            Student student = new Student
+            {
+                Id = 1,
+                FirstName = "Müge",
+                LastName = "Özkan",
+                Department = "Archeology"
+
+            };
+            manager.Add(student);
+        }
+    }
+
+    interface IPerson
+    {
+        int Id { get; set; }
+        string FirstName { get; set; }
+        string LastName { get; set; }
+
+    }
+
+    class Student : IPerson
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Department { get; set; }
+
+    }
+    class Customer : IPerson
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+    }
+    class PersonManager
+    {
+        /*burada add komutu ile interface değil normal class tanımlasaydım,
+        yukarıda çağırma işleminde class ismi uyuşmadığı takdirde hata alacaktım.*/
+        public void Add(IPerson person)
+        {
+            Console.WriteLine(person.FirstName);
+        }
+    }
+}
